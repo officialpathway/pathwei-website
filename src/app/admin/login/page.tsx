7a7614ef3bc5c2c0357bc4dbc30dc6e8ce2b1cb8
Alvaro Rios <alvaroriro2003@gmail.com>
2025-06-19 13:45:31 +0200

    second fix

--- src/app/admin/login/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import LoginPage from '@/components/client/admin/LoginPage';
-
-// This is a standalone page that does not use the admin layout
-export default function AdminLoginPage() {
-  return <LoginPage />;
-}
